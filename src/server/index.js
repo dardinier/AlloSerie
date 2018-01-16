e124e1acab610b4b3ff5104cf27bf6e1230d8a06
Alexis Dardinier <alexis.dardinier@etu.udamail.fr>
2018-01-16 16:06:49 +0100

    Added server start

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,8 @@
 const express = require('express');
 const api = require('./api.js');
 const config = require('./config.js');
+
+const app = express();
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`Server started at localhost:${PORT}`));
