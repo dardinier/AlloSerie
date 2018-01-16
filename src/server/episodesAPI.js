35b8a4a84e2d919a3e31cc052c684507c57293f1
Alexis Dardinier <alexis.dardinier@etu.udamail.fr>
2018-01-16 17:31:01 +0100

    POST /episodes create a file with the body parameters

--- src/server/episodesAPI.js
+++ src/server/episodesAPI.js
@@ -1,10 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const config = require('./config');
+const express = require('express');
+const router = express.Router();
 const uuidv4 = require('uuid/v4');
+const jsonfile = require('jsonfile');
 
+/**
+ * Create an episode
+ */
 router.post('/', function(req, res) {
     let body = req.body;
 
+    /* Test if parameters are well setted */
     if (typeof body.name !== "string" || typeof body.code !== "string" || typeof body.score !== "number") {
         res.status(400).end();
     }
@@ -12,9 +18,13 @@ router.post('/', function(req, res) {
     /* Generate a random UID */
     body.id = uuidv4();
 
-    /** TO-DO
-     * Create the file of the episode
-     */
+    /* Construct the URL of the file */
+    const file = config.data + "/episode." + body.id + ".json";
+
+    /* Write the file according the body */
+    jsonfile.writeFile(file, body, function () {
+        res.status(400).end();
+    });
 
     res.status(201);
     res.send(body);
--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const episodesAPI = require('./episodesAPI.js');
 const config = require('./config.js');
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
 const app = express();
 
