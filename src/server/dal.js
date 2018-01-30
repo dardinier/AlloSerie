554432f5040ce6f5e35560809e37a4a36e1c890d
Alexis Dardinier <alexis.dardinier@etu.udamail.fr>
2018-01-30 17:34:19 +0100

    Added DAL and review of episodes API

---
+++ src/server/dal.js
@@ -0,0 +1,90 @@
+const fs = require('fs');
+const path = require('path');
+const config = require('./config');
+const uuidv4 = require("uuid/v4");
+
+const readFile = (filePath) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path.join(config.data, filePath), "utf8", (error, data) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+            resolve(JSON.parse(data));
+        });
+    });
+};
+
+const writeFile = (data) => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(path.join(config.data, "episode." + data.id + ".json"), JSON.stringify(data), "utf8", (error) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+            resolve(data);
+        });
+    });
+};
+
+const deleteFile = (filePath) => {
+    return new Promise((resolve, reject) => {
+        fs.unlink(path.join(config.data, filePath), (error) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+            resolve();
+        });
+    });
+};
+
+exports.findAll = () => {
+    return new Promise((resolve, reject) => {
+        fs.readdir(config.data, (error, files) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+            resolve(Promise.all(files.map((file) => {
+                return readFile(file);
+            })));
+        });
+    });
+};
+
+exports.findOne = (id) => {
+    return new Promise((resolve, reject) => {
+        fs.readdir(config.data, (error, files) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+            resolve(readFile(files.filter(file => file == "episode." + id + ".json")[0]));
+        });
+    });
+};
+
+exports.insert = (data) => {
+    data.id = uuidv4();
+    return new Promise((resolve, reject) => {
+        writeFile(data).then((data) => {
+            resolve(data);
+        })
+            .catch((error) => {
+                reject(error);
+            });
+    });
+};
+
+exports.delete = (id) => {
+    return new Promise((resolve, reject) => {
+        fs.readdir(config.data, (error, files) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+            resolve(deleteFile(files.filter(file => file == "episode." + id + ".json")[0]));
+        });
+    });
+};
--- src/server/episodesAPI.js
+++ src/server/episodesAPI.js
@@ -1,14 +1,13 @@
 const config = require('./config');
 const express = require('express');
-const fs = require('fs');
 const router = express.Router();
-const uuidv4 = require('uuid/v4');
-const jsonfile = require('jsonfile');
+const dal = require('./dal');
 
 /**
  * Create an episode
  */
 router.post('/', function(req, res) {
+
     let body = req.body;
 
     /* Test if parameters are well setted */
@@ -17,158 +16,109 @@ router.post('/', function(req, res) {
         return;
     }
 
-    /* Test if there is only 3 attributes */
-    const keys = Object.keys(body);
-    if (keys.length != 3) {
-        res.status(400).end();
-        return;
-    }
-
-    /* Generate a random UID */
-    body.id = uuidv4();
-
-    /* Construct the URL of the file */
-    const file = config.data + "/episode." + body.id + ".json";
-
-    /* Write the file according the body */
-    jsonfile.writeFile(file, body, {spaces: 2, EOL: '\r\n'}, function () {
-        res.status(400).end();
-    });
-
-    /* Send the created episode */
-    res.status(201);
-    res.send(body);
+    /* Insert the new episode */
+    dal.insert(body)
+        .then((episode) => {
+            res.status(201);
+            res.send(episode);
+        })
+        .catch(() => {
+            res.status(400).end();
+        })
 });
 
 /**
  * Get all episodes
  */
 router.get('/', function(req, res) {
-
-    /* Get all files names from data directory */
-    let episodesFiles = fs.readdirSync(config.data);
-
-    /* Put all episodes in an array */
-    let episodes = [];
-    episodesFiles.forEach(episodeFile => {
-        episodes.push(jsonfile.readFileSync(config.data + "/" + episodeFile));
-    }, episodes);
-
-    /* Send the episodes */
-    res.status(200);
-    res.send(episodes);
+    dal.findAll()
+        .then((episodes) => {
+            res.status(200);
+            res.send(episodes);
+        })
+        .catch(() => {
+            res.status(404).end();
+        });
 });
 
 /**
  * Get an episode
  */
 router.get('/:id', function(req, res) {
-
-    /* Get all files names from data directory */
-    let episodesFiles = fs.readdirSync(config.data);
-
-    /* Get the episode */
-    let episode = null;
-    episodesFiles.forEach(episodeFile => {
-        if(episodeFile == "episode." + req.params.id + ".json") {
-            episode = jsonfile.readFileSync(config.data + "/" + episodeFile);
-        }
-    }, episode);
-
-    /* If the episode if not found, throw an error */
-    if(episode == null) {
-        res.status(404).end();
-    }
-
-    /* Send the episode */
-    res.status(200);
-    res.send(episode);
+    dal.findOne(req.params.id)
+        .then((episode) => {
+            res.status(200);
+            res.send(episode);
+        })
+        .catch(() => {
+            res.status(404).end();
+        });
 });
 
 /**
  * Delete an episode
  */
 router.delete('/:id', function(req, res) {
-
-    /* Get all files names from data directory */
-    let episodesFiles = fs.readdirSync(config.data);
-
-    /* Delete the episode */
-    episodesFiles.forEach(episodeFile => {
-        if(episodeFile == "episode." + req.params.id + ".json") {
-            let returnCode = fs.unlinkSync(config.data + "/" + episodeFile);
-            if(returnCode == undefined) {
-                res.status(204).end();
-            }
-            else {
-                res.status(400).end();
-            }
-        }
-    });
-    /* If no episode matchs, send an error */
-    res.status(404).end();
+    dal.delete(req.params.id)
+        .then(() => {
+            res.status(204).end();
+        })
+        .catch(() => {
+            res.status(404).end();
+        });
 });
 
 /**
  * Update an episode
  */
 router.put('/:id', function(req, res) {
+
     let body = req.body;
-    let episodesFiles = fs.readdirSync(config.data);
-    let episode = null;
-    let file = null;
 
-    /* Get the episode */
-    episodesFiles.forEach(episodeFile => {
-        if(episodeFile == "episode." + req.params.id + ".json") {
-            episode = jsonfile.readFileSync(config.data + "/" + episodeFile);
-            file = config.data + "/" + episodeFile;
-        }
-    }, episode, file);
+    dal.findOne(req.params.id)
+        .then((episode) => {
+            if(body.name != null) {
+                if (typeof body.name == "string") {
+                    episode.name = body.name;
+                } else {
+                    res.status(400).end();
+                }
+            }
 
-    /* If the episode is not found, send an error */
-    if(episode == null) {
-        res.status(404).end();
-        return;
-    }
+            if(body.code != null) {
+                if (typeof body.code == "string") {
+                    episode.code = body.code;
+                } else {
+                    res.status(400).end();
+                }
+            }
 
-    /* Update the name of the episode */
-    if(body.name != null) {
-        if (typeof body.name == "string") {
-            episode.name = body.name;
-        } else {
-            res.status(400).end();
-        }
-    }
+            if(body.score != null) {
+                if (typeof body.score == "number") {
+                    episode.score = body.score;
+                } else {
+                    res.status(400).end();
+                }
+            }
 
-    /* Update the code of the episode */
-    if(body.code != null) {
-        if (typeof body.code == "string") {
-            episode.code = body.code;
-        } else {
-            res.status(400).end();
-        }
-    }
-
-    /* Update the score of the episode */
-    if(body.score != null) {
-        if (typeof body.score == "number") {
-            episode.score = body.score;
-        } else {
-            res.status(400).end();
-        }
-    }
-
-    /* Rewrite the episode */
-    if(file != null) {
-        jsonfile.writeFile(file, episode, {spaces: 2, EOL: '\r\n'}, function () {
-            res.status(400).end();
+            dal.delete(req.params.id)
+                .then(() => {
+                    dal.insert(episode)
+                        .then((episode) => {
+                            res.status(200);
+                            res.send(episode);
+                        })
+                        .catch(() => {
+                            res.status(400).end();
+                        });
+                })
+                .catch(() => {
+                    res.status(400).end();
+                });
+        })
+        .catch(() => {
+            res.status(404).end();
         });
-    }
-
-    /* Send the updated episode */
-    res.status(200);
-    res.send(episode);
 });
 
 module.exports = router;
